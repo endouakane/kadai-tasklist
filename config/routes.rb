d9a7c7738ad740e1d53feffed888ac3663c24149
akaneendo <chidoriayumu@gmail.com>
2017-06-12 09:27:45 +0000

    20170612

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'toppages/index'
 
-  resources :tasks
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  root to: "toppages#index"
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get "signup" , to: "users#new"
+  
+  resources :tasks, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create]
 end
